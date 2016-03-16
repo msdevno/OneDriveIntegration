315722a03846aa53def44e3722f176bebd84d7d1
Einar Ingebrigtsen <einar@dolittle.com>
2016-03-16 15:16:17 +0100

    Everything up and running

---
+++ OneDriveIntegration/GetFile.ashx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace OneDriveIntegration
+{
+    /// <summary>
+    /// Summary description for GetFile
+    /// </summary>
+    public class GetFile : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "Application/binary";
+
+            var path = context.Server.MapPath("~/index.html");
+            context.Response.WriteFile(path);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ OneDriveIntegration/UploadFile.ashx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IdentityModel.Tokens;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace OneDriveIntegration
+{
+    /// <summary>
+    /// Summary description for UploadFile
+    /// </summary>
+    public class UploadFile : IHttpHandler
+    {
+        public void ProcessRequest(HttpContext context)
+        {
+            // Get the things from the request body
+            var accessToken = context.Request.Params["accessToken"];
+            var folderId = context.Request.Params["folderId"];
+
+            // Decode the JWT access token and get the tenant information (aud)
+            var token = new JwtSecurityToken(accessToken);
+            var tenant = token.Claims.Where(c => c.Type == "aud").Single();
+            var tenantUrl = tenant.Value;
+
+            var filename = "index.html";
+            var path = context.Server.MapPath($"~/{filename}");
+
+            // Build the API URL
+            var baseUrl = $"{tenant.Value}/_api/v2.0/me/";
+            var url = $"{baseUrl}drive/items/{folderId}/children/{filename}/content";
+
+            using ( var client = new WebClient())
+            {
+                client.Headers["Authorization"] = "bearer "+accessToken;
+                
+                try {
+
+                    // You could do an async here...
+                    client.UploadFile(url, "PUT", path);
+                } catch (Exception ex)
+                {
+                    var i = 0;
+                    i++;
+                }
+            }
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
